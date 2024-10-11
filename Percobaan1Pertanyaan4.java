6fbb26df0decbc6fc80f6204e36d240ac0be19ac
Erilocshada <erilleo23@gmail.com>
2024-10-11 08:56:00 +0700

    Percobaan1 Pertanyaan4

--- Percobaan1Pertanyaan4.java
+++ Percobaan1Pertanyaan4.java
@@ -1,10 +1,11 @@
 
 import java.util.Scanner;
 
-public class SiakadFor20 {
+public class Percobaan1Pertanyaan4 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-    double nilai, tertinggi = 0, terendah = 100;
+    double nilai, terendah = 100, tertinggi = 0;
+    int lulus = 0, tidaklulus = 0;
 
     for (int i = 1; i < 10; i++) {
         System.out.println("Masukkan nilai mahasiswa-" + i+ ":");
@@ -15,7 +16,15 @@ public class SiakadFor20 {
         if (nilai < terendah) {
             terendah = nilai;
         }
+        if (nilai < 60) {
+            tidaklulus++;
+        } else if (nilai >= 60) {
+            lulus++;
+        }
+
     }
+    System.out.println("banyaknya mahasiswa lulus : " + lulus);
+    System.out.println("banyaknya mahasiswa tidak lulus : " + tidaklulus);
     System.out.println("nilai tertinggi :" + tertinggi);
     System.out.println("nilai terendah :" + terendah);
     sc.close();
